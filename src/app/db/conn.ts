cff81eaf266680bb162ea4d9fa2761e840898179
Martin H. Pefaur <mpefaur@gmail.com>
2024-03-11 15:29:15 -0300

    Add env.example

--- src/app/db/conn.ts
+++ src/app/db/conn.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-export const connection = async() => {
+export const connection = async () => {
   try {
-    await mongoose.connect(`Aca-va-la-URL-a-tu-MongoDB-Atlas`, {
-    });
+    const mongoDBUrl = process.env.MONGODB_URL;
+    if (!mongoDBUrl) {
+      throw new Error("MongoDB URL is not defined in environment variables");
+    }
+    await mongoose.connect(mongoDBUrl);
     console.log("Connection successful with MongoDB");
   } catch (err) {
     console.error("Connection failed with MongoDB:", err);
   }
 };
-
